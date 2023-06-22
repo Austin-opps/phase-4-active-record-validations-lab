a55b6e9a500e2718d4aeb3218007bf701c5d18e7
Austin <akomen04@gmail.com>
2023-06-22 10:51:47 +0300

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+
+  validate :validate_title_not_true_facts
+
+  private
+
+  def validate_title_not_true_facts
+    errors.add(:title, "cannot be 'True Facts'") if title == "True Facts"
+  end
 end
